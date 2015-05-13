d892a153fdd750d7537757d1677fc5d999cc39d0
Tyler Harned <trharned@fullsail.edu>
2015-05-12 20:03:22 -0500

    code.js coding
    Coded the code.js to finish the circumference calculator.

--- Expression Worksheet/Circle/js/code.js
+++ Expression Worksheet/Circle/js/code.js
@@ -1,3 +1,17 @@
-/**
- * Created by tylerharned on 5/12/15.
+/*
+Tyler Harned
+05/12/2015
+Full Sail University - SDI #03
+Expressions Worksheet
+Circumference of Circle Calculator
  */
+
+// Calculate the Circumference of a Circle
+var radius = 6;
+const PI = 3.14159265;
+
+// Multiply 2, PI and the radius & assign to circ variable.
+var circ = radius * 2 * PI;
+
+// Print out the output circumference
+console.log("The circumference of the circle is" +circ);
