0d765f67adc6843da83584a286bcd4784986628b
Tyler Harned <trharned@fullsail.edu>
2015-05-13 18:06:59 -0500

    Created the DogYears base files
    Created the base files for DogYears calculator. Meaning, i’ve created
    the js file and the html file. I’ve then coded the calculator to run
    when Sparky is 2 years old in human years.

---
+++ Expression Worksheet/Dog Years/js/sparky.js
@@ -0,0 +1,17 @@
+/*
+ Tyler Harned
+ 05/13/2015
+ Full Sail University - SDI #03
+ Expressions Worksheet
+ Dog Years Calculator
+ */
+
+// Calculate Dog Years
+var Sparky = 2;
+const years = 7;
+
+// Multiply 2, PI and the radius & assign to circ variable.
+var dogYears = Sparky * years;
+
+// Print out the output circumference
+console.log("Sparky is "+Sparky+" which is "+dogYears+" in dog years.");
