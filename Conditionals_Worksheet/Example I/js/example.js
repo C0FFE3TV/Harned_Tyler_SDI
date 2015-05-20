f0f38a2098cba041b98035ac120ed3e5e60a945c
Tyler Harned <trharned@fullsail.edu>
2015-05-20 18:26:11 -0500

    javascript coded
    Coded the beach or the movies example code. Added a prompt and
    console.log’s.

--- Conditionals_Worksheet/Example I/js/example.js
+++ Conditionals_Worksheet/Example I/js/example.js
@@ -1,3 +1,21 @@
-/**
- * Created by tylerharned on 5/20/15.
+/*
+Tyler Harned
+Conditionals Worksheet
+Full Sail - SDI #03
+05/20/2015
+Example I
  */
+
+//Hot Enough?
+
+// Is it hot enough to go to the beach?
+var temp = prompt("What is the temperature? ");
+
+//if the temperature is less than 75..
+if(temp < 75){
+//if its less than 75
+console.log("We will go to the beach!");
+}else{
+//if it’s greater or equal to 75<br>
+console.log("We will go to the movies!");
+}
