bf55c5863750b48b11e3f78453c8f32512b6bd53
Tyler Harned <trharned@fullsail.edu>
2015-05-26 23:55:33 -0500

    script.js edited
    Added more notes by using //’s
    Setup alerts to alert the user what code is running.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,31 +6,30 @@
  */
 
 // Circle Circumference
-console.log("Now running code for Circumference Calculator:"); // Let the user know what code is running
+alert("Now running code for Circumference Calculator:"); // Alert the user to let them know what code is running
+console.log("Now running code for Circumference Calculator:"); // Log it in the console.
 
-// Create the function
-var radius = prompt("What is the radius?");
-var circumference = calcCircumference(radius);
-console.log("The Circumference of the circle is "+circumference);
+// Calculations
+var radius = prompt("What is the radius?"); // Gather radius information
+var circumference = calcCircumference(radius); // Create the function
+console.log("The Circumference of the circle is "+circumference); // Log the results
 
-function calcCircumference(radius)
+function calcCircumference(radius) // Create the parameters
 {
     return 2 * 3.14 * radius; // Return the function
 }
 
 // Bee Stings
-alert("Now running code for Bee Stings.");
-console.log("Now running code for Bee Stings."); // Let the user know what code is running
+alert("Now running code for Bee Stings."); // Let the user know what code is running
+console.log("Now running code for Bee Stings."); // Log what code is running in the console.
 
-// Create the function
+// Calculations
 var weight = prompt("How much does the animal weigh?"); // Gather information
-var beesting = calcBeeSting(weight); // Create a parameter
+var beesting = calcBeeSting(weight); // Create the function
 console.log("It takes "+beesting+" bee stings to kill this animal."); // Log the results
 
-function calcBeeSting(weight) // Function?
+function calcBeeSting(weight) // Create the parameters
 {
     return 8.666666667 * weight; // Return the function
 }
-
-//
 
