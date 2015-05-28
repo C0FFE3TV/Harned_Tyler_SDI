8c0cfea460ba471a58a0059cbb131b955a4cb319
Tyler Harned <trharned@fullsail.edu>
2015-05-28 00:15:32 -0500

    Minimum number and validation
    Asked the user for a minimum number and validated to make sure that the
    user used a number and didn’t leave blank or a letter.

--- GoTo Training Week4/js/script.js
+++ GoTo Training Week4/js/script.js
@@ -8,3 +8,32 @@ GoTo Training Week 4
 
 // alert("Testing to see if connected.");
 // console.log("Testing to see if connected.");
+
+// Create code that will give us a random number between two numbers.
+// Create a function that will do it and return that value.
+
+// Ask the user for a minimum number
+var min = prompt("Let's get a random number between two numbers.\nPlease type in a minimum number:");
+
+/*
+Outside of while loop
+ - set a variable
+   var x = 6;
+ - create the while loop
+   while(condition to test){
+    OTHER CODE
+    change to the variable, this avoids an infinite loop.
+   }
+ */
+
+// Validate that prompt
+while(min==="" || isNaN(min)){
+    // This will run if conditional inside of () is true
+    // re-prompt the user with the same variable name
+    if(min===""){
+        min=prompt("Please do not leave blank.\nPlease type in a minimum number:");
+    }else{
+        min=prompt("Please only use numbers.\nPlease type in a minimum number:");
+    }
+}
+
